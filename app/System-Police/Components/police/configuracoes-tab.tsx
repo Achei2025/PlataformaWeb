10244af49110ff86a75c4daff6310acac13ebe4e
Neto <luizbarros200303@gmail.com>
2025-03-12 09:45:03 -0300

    Adicionando arquivos e lincença gpl 3.0

--- app/System-Police/Components/police/configuracoes-tab.tsx
+++ app/System-Police/Components/police/configuracoes-tab.tsx
@@ -1,3 +1,26 @@
+/*
+ * Achei: Stolen Object Tracking System.
+ * Copyright (C) 2025  Team Achei
+ * 
+ * This file is part of Achei.
+ * 
+ * Achei is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Achei is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Achei.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * Contact information: teamachei.2024@gmail.com
+*/
+
+
 "use client"
 
 import type React from "react"
